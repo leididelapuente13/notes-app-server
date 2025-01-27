e8836784cb7512ab71bc25a2ce5445cae060cf6c
Leidi <ldelapuente.sysnet@outlook.com>
2025-01-27 10:46:28 -0500

    feat(note.schema, tag.schema): create note and tag schema

---
+++ src/domain/schema/tag.schema.ts
@@ -0,0 +1,8 @@
+import z from 'zod';
+import { TagEntity } from '../entities';
+
+export const TagSchema = z.object({
+    name: z.string({message: 'It should be a string'}).min(1, {message: 'This is required'}),
+});
+
+export const validateTag = (tag: TagEntity)=> TagSchema.safeParse(tag);
